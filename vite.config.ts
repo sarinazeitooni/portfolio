3e670fad2845643777d13461f639022648c24c63
zeitoonisarina <zeitoonisarina@gmail.com>
2026-09-19 12:18:21 +0330

    feat: changed the base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // Served from https://sarinazeitooni.github.io/portfolio/
 export default defineConfig({
-  base: "/portfolio/",
+  base: "/",
   plugins: [react()],
 });
